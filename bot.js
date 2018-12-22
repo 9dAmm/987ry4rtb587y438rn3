ee4e7c53dec6e203cbbe5a8db76f76a552dcc45a
9dAmm <46056715+9dAmm@users.noreply.github.com>
2018-12-22 10:13:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const ownerID = '346629187504832513';
 
 client.on('ready', () => {
-  console.log(client.user.tag + ' Ready! (' + client.user.id + ')');
+    console.log(client.user.tag + ' Ready! (' + client.user.id + ')');
+    client.user.setActivity('- 9dAmm , FM', "https://www.twitch.tv/unkown");
 });
 
 client.login(process.env.TOKEN);
