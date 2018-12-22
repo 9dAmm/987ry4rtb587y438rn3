89d8a7aa469a031f82ec8f8dbd9925497d601ed2
9dAmm <46056715+9dAmm@users.noreply.github.com>
2018-12-23 00:10:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -57,8 +57,8 @@ client.on("message", message => {
  if (message.content.startsWith(prefix + "bc")) {
   if (!message.member.hasPermission("ADMINISTRATOR"))  return;
    let args = message.content.split(" ").slice(1);
-    var argresult = args.join (' ');
-     message.guild.members.filter (m => m.presence.statue !== 'offline').forEach(m => {
+    var argresult = args.join(' ');
+     message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
 	  m.send('${argresult}\n ${m}');
 })
 message.channel.send ('\'${message.guild.members.filter(m => m.presence.status !== 'online').size}\'عدد الأعضاء اللذين تم ارسال الرسالة لهم :');
