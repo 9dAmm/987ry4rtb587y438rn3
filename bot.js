421d638d41d5ff62ad56917abca52f55f511f686
9dAmm <46056715+9dAmm@users.noreply.github.com>
2018-12-22 10:17:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const ownerID = '346629187504832513';
 
 client.on('ready', () => {
     console.log(client.user.tag + ' Ready! (' + client.user.id + ')');
-    client.user.setActivity('- 9dAmm , FM', "https://www.twitch.tv/unkown");
+    client.user.setActivity('- 9dAmm , FM', "https://www.twitch.tv/xiaboodz_");
 });
 
 client.login(process.env.TOKEN);
