60cc4d2ca3087bfb3805df92742d383d4e748f56
9dAmm <46056715+9dAmm@users.noreply.github.com>
2018-12-23 00:12:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -52,18 +52,4 @@ client.on('message', message => {
 });
 
 
-client.on("message", message => {
- 
- if (message.content.startsWith(prefix + "bc")) {
-  if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-   let args = message.content.split(" ").slice(1);
-    var argresult = args.join(' ');
-     message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
-	  m.send('${argresult}\n ${m}');
-})
-message.channel.send ('\'${message.guild.members.filter(m => m.presence.status !== 'online').size}\'عدد الأعضاء اللذين تم ارسال الرسالة لهم :');
-message.delete();
-};
-});
-
 client.login(process.env.TOKEN);
