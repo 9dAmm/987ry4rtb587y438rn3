32f955fae9d50afb154242f74cb1121f55cb9049
9dAmm <46056715+9dAmm@users.noreply.github.com>
2018-12-22 11:33:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,4 +10,39 @@ client.on('ready', () => {
     });
 });
 
+client.on('message', message => {
+    if(message.channel.type !== 'text') return;
+    if(message.author.id !== ownerID) return;
+    
+    var args = message.content.toLowerCase().split(' ');
+    var command = args[0];
+    var prefix = '9';
+    
+    if(command == prefix + 'help') {
+        
+    }
+    if(command == prefix + 'setname') {
+        args = message.content.split(' ').slice(1).join(' ');
+        if(args) return;
+        if(args2.length > 32) return;
+        client.user.setUsername(args);
+        message.channel.send(`Successfully set my name to **${args}**`);
+    }
+    if(command == prefix + 'setavatar') {
+        
+    }
+    if(command == prefix + 'setplay') {
+        
+    }
+    if(command == prefix + 'setlisten') {
+        
+    }
+    if(command == prefix + 'setwatch') {
+        
+    }
+    if(command == prefix + 'setstream') {
+        
+    }
+});
+
 client.login(process.env.TOKEN);
