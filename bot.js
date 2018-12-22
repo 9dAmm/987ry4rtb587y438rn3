1211d814c51929470a6822413519ff15aa3591c6
9dAmm <46056715+9dAmm@users.noreply.github.com>
2018-12-22 23:54:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -51,4 +51,30 @@ client.on('message', message => {
     }
 });
 
+const Discord = require ("discord.js"):
+const client = new Discord.Client():
+var prefix = "9"
+client.on("message", message => {
+ 
+ if (message.content.startWith(prefix + "bc")) {
+  if (!message.member.hasPermission(ADMINISTRATOR"))  return;
+   let args = message.content.split(" ").slice(1);
+    var argresult = args.join (' ');
+     message.guild.members.filter (m => m.presence.statue !== 'offline').forEach(m => {
+	  m.send('${argresult}\n ${m}');
+})
+message.channel.send ('\'${message.guild.members.filter(m => m.presence.status !== 'online').size}\'عدد الأعضاء اللذين تم ارسال الرسالة لهم :');
+message.delete();
+};
+});
+client.on('ready', () => {
+console.log('<--------------->');
+console.log(' ');
+console.log('');
+console.log('Dev: Khalid Alenazi');
+console.log(' ');
+console.log(' ');
+console.log('<--------------->');
+});
+
 client.login(process.env.TOKEN);
