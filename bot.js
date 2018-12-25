6eb070760d857a008aeb80bc7f3b7a4d3773f6cf
9dAmm <46056715+9dAmm@users.noreply.github.com>
2018-12-25 23:34:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,52 +3,52 @@ const client = new Discord.Client();
 const ownerID = '325165115131428864';
 
 client.on('ready', () => {
-	console.log(client.user.tag + ' Ready! (' + client.user.id + ')');
-	client.user.setActivity('?????? ???n?z? ?', {
-		type: "STREAMING",
-		url: "https://www.twitch.tv/unkown"
-	});
+    console.log(client.user.tag + ' Ready! (' + client.user.id + ')');
+    client.user.setActivity('?????? ???n?z? ?', {
+        type: "STREAMING",
+        url: "https://www.twitch.tv/unkown"
+    });
 });
 
 client.on('message', message => {
-	if(message.channel.type !== 'text') return;
-	if(message.author.id !== ownerID) return;
-	
-	var args = message.content.toLowerCase().split(' ');
-	var command = args[0];
-	var prefix = '9';
-	
-	if(command == prefix + 'help') {
-		
-		}
-	if(command == prefix + 'setname') {
- 		args = message.content.split(' ').slice(1).join(' ');
-		if(args) return;
-		if(args.length > 32) return;
-		client.user.setUsername(args);
-		message.channel.send(`Successfully set my name to ${args}`);
-	}
-	if(command == prefix + 'setavatar') {
-		
-	}
-	if(command == prefix + 'setplay') {
-		
-	}
-	if(command == prefix + 'setlisten') {
-		
-	}
-	if(command == prefix + 'setwatch') {
-		
-	}
-	if(command == prefix + 'setstream') {
-		
-	}
-	if(command == prefix + 'say') {
-		args = message.content.split(' ').slice(1).join(' ');
-		if(!args) return message.delete();
-		message.delete();
-		message.channel.send(args);
-	}
+    if(message.channel.type !== 'text') return;
+    if(message.author.id !== ownerID) return;
+    
+    var args = message.content.toLowerCase().split(' ');
+    var command = args[0];
+    var prefix = '9';
+    
+    if(command == prefix + 'help') {
+        
+    }
+    if(command == prefix + 'setname') {
+        args = message.content.split(' ').slice(1).join(' ');
+        if(args) return;
+        if(args2.length > 32) return;
+        client.user.setUsername(args);
+        message.channel.send(Successfully set my name to ${args});
+    }
+    if(command == prefix + 'setavatar') {
+        
+    }
+    if(command == prefix + 'setplay') {
+        
+    }
+    if(command == prefix + 'setlisten') {
+        
+    }
+    if(command == prefix + 'setwatch') {
+        
+    }
+    if(command == prefix + 'setstream') {
+        
+    }
+    if(command == prefix + 'say') {
+        args = message.content.split(' ').slice(1).join(' ');
+        if(!args) return message.delete();
+        message.delete();
+        message.channel.send(args);
+    }
 	if(command == prefix + 'bc') {
 		args = message.content.split(' ').slice(1).join(' ');
 		if(!args) return;
